9d251837c694095560407822e260eb0b9135f335
srani691 <s.rani.15@student.scu.edu.au>
2024-07-09 10:44:37 +0500

    Initial version of task
    This commit is for initial version

---
+++ StudentMarks/StudentMarks.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class StudentMarks here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class StudentMarks
+{
+public static void main(String[] args){
+    //creating input for taking input values
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter assignment name:");
+    String assignmentName=input.nextLine();
+   
+    
+    
+}
+}
